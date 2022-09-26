b086f891fc9811a1d2fffac58a7ae6dc9637cb94
Jomon T Jose <jomon.jose1605@gmail.com>
2022-09-26 09:13:19 +0530

    changes

--- src/Employee/employee.controller.ts
+++ src/Employee/employee.controller.ts
@@ -1,12 +1,14 @@
 import {
-  Get,
-  Post,
-  Controller,
-  Res,
-  HttpStatus,
-  Body,
-  Param,
-  Put,
+	Get,
+	Post,
+	Controller,
+	Res,
+	HttpStatus,
+	Body,
+	Param,
+	Put,
+	Query,
+	Delete,
 } from '@nestjs/common';
 import { AuthService } from '../Auth/auth.service';
 import { UpdateEmployeeDto } from '../Employee/update-employee.dto';
@@ -15,87 +17,122 @@ import { EmployeeService } from './employee.service';
 
 @Controller('employee')
 export class EmployeeController {
-  constructor(
-    private readonly employeeService: EmployeeService,
-    private readonly authService: AuthService,
-  ) {}
+	constructor(
+		private readonly employeeService: EmployeeService,
+		private readonly authService: AuthService,
+	) { }
 
-  @Get()
-  async getEmployees(@Res() response) {
-    try {
-      const employeeList = await this.employeeService.getAllEmployees();
-      return response.status(HttpStatus.OK).json({
-        message: 'All Employees fetched',
-        employeeList,
-      });
-    } catch (err) {
-      return response.status(err.status).json(err.response);
-    }
-  }
+	@Get()
+	async getEmployees(@Res() response) {
+		try {
+			const employeeList = await this.employeeService.getAllEmployees();
+			return response.status(HttpStatus.OK).json({
+				message: 'All Employees fetched',
+				employeeList,
+			});
+		} catch (err) {
+			return response.status(err.status).json(err.response);
+		}
+	}
 
-  @Post()
-  async createEmployee(
-    @Res() response,
-    @Body() createEmployeeDto: CreateEmployeeDTO,
-  ) {
-    try {
-      const newEmployee = await this.employeeService.createEmployee(
-        createEmployeeDto,
-      );
-      const payload = {
-        email: newEmployee.email,
-      };
-      const token = await this.authService.signPayload(payload);
-      const response1 = { newEmployee, token };
-      return response.status(HttpStatus.CREATED).json({
-        message: 'New Employee Created',
-        response1,
-      });
-    } catch (err) {
-      return response.status(err.status).json({
-        statusCode: 400,
-        message: 'Error: Employee not created!',
-        error: 'Bad Request',
-      });
-    }
-  }
+	@Post()
+	async createEmployee(
+		@Res() response,
+		@Body() createEmployeeDto: CreateEmployeeDTO,
+	) {
+		try {
+			const newEmployee = await this.employeeService.createEmployee(
+				createEmployeeDto,
+			);
+			const payload = {
+				email: newEmployee.email,
+			};
+			const token = await this.authService.signPayload(payload);
+			const employee = { newEmployee, token };
+			return response.status(HttpStatus.OK).json({
+				message: 'New Employee Created',
+				employee,
+			});
+		} catch (err) {
+			console.log(err.message)
+			return response.status(err.status).json({
+				statusCode: 400,
+				message: err.message,
+				error: 'Bad Request',
+			});
+		}
+	}
 
-  @Get('/:email')
-  async getEmployeesbyEmail(@Res() response, @Param('email') email: string) {
-    try {
-      const employeeList = await this.employeeService.getEmployeesByEmail(
-        email,
-      );
-      return response.status(HttpStatus.OK).json({
-        message: 'Employee fetched by EmailId',
-        employeeList,
-      });
-    } catch (err) {
-      return response.status(err.status).json(err.response);
-    }
-  }
+	@Get('byemail')
+	async getEmployeesbyEmail(@Res() response, @Query('email') email: string) {
+		try {
+			const employeeList = await this.employeeService.getEmployeesByEmail(
+				email,
+			);
+			if (employeeList) {
+				return response.status(HttpStatus.OK).json({
+					message: 'Employee fetched by EmailId',
+					employeeList,
+				});
+			} else {
+				return response.status(HttpStatus.OK).json({
+					message: 'No Employee exists',
+					employeeList,
+				});
+			}
 
-  @Put()
-  async updateEmployee(
-    @Res() response,
-    @Body() updateEmployeeDto: UpdateEmployeeDto,
-  ) {
-    try {
-      const newEmployee = await this.employeeService.updateEmployee(
-        updateEmployeeDto,
-        updateEmployeeDto.email,
-      );
-      console.log('Updated Epm ' + newEmployee);
-      return response.status(HttpStatus.CREATED).json({
-        message: 'New Employee Created',
-        newEmployee,
-      });
-    } catch (err) {
-      return response.status(err.status).json({
-        statusCode: 400,
-        message: 'Error: Employee not created!',
-        error: 'Bad Request',
-      });
-    }
-  }
+		} catch (err) {
+			return response.status(err.status).json(err.response);
+		}
+	}
+
+	@Put()
+	async updateEmployee(
+		@Res() response,
+		@Body() updateEmployeeDto: UpdateEmployeeDto,
+	) {
+		try {
+			const newEmployee = await this.employeeService.updateEmployee(
+				updateEmployeeDto,
+				updateEmployeeDto.email,
+			);
+			console.log('Updated Epm ' + newEmployee);
+			return response.status(HttpStatus.CREATED).json({
+				message: 'New Employee Created',
+				newEmployee,
+			});
+		} catch (err) {
+			return response.status(err.status).json({
+				statusCode: 400,
+				message: 'Error: Employee not created!',
+				error: 'Bad Request',
+			});
+		}
+	}
+
+	@Delete()
+	async deleteEmployee(
+		@Res() response,
+		@Query('email') email: string
+	) {
+		try {
+			console.log(email);
+			const deleted = await this.employeeService.deleteEmployee(
+				email
+			);
+			console.log('Deleted Employee ' + deleted);
+			return response.status(HttpStatus.CREATED).json({
+				message: 'Employee Deleted',
+				deleted,
+			});
+		} catch (err) {
+			console.log(err);
+			return response.status(err.status).json({
+				statusCode: 400,
+				message: 'Error: Employee not created!',
+				error: 'Bad Request',
+			});
+		}
+
+	}
 }
--- src/Employee/employee.service.ts
+++ src/Employee/employee.service.ts
@@ -9,93 +9,122 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class EmployeeService {
-  constructor(
-    @InjectModel('employees')
-    private employeeModel: Model<IEmployee>,
-  ) {}
+	constructor(
+		@InjectModel('employees')
+		private employeeModel: Model<IEmployee>,
+	) { }
 
-  async getAllEmployees() {
-    try {
-      const employeeList = await this.employeeModel.find().exec();
-      return employeeList.map((employees) => ({
-        firstname: employees.firstName,
-        lastName: employees.lastName,
-        email: employees.email,
-        address1: employees.address1,
-        address2: employees.address2,
-        location: employees.location,
-        country: employees.country,
-      }));
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	async getAllEmployees() {
+		try {
+			const employeeList = await this.employeeModel.find().exec();
+			return employeeList.map((employees) => ({
+				firstname: employees.firstName,
+				lastName: employees.lastName,
+				email: employees.email,
+				address1: employees.address1,
+				address2: employees.address2,
+				location: employees.location,
+				country: employees.country,
+				role: employees.role,
+				id: employees._id
+			}));
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-  async getEmployeesByEmail(mail: string) {
-    const employee: unknown = await this.employeeModel.findOne({ email: mail });
-    const resEmp: IEmployee = employee as IEmployee;
-    const existingEmployee = {
-      firstName: resEmp.firstName,
-      lastName: resEmp.lastName,
-      email: resEmp.email,
-      address1: resEmp.address1,
-      address2: resEmp.address2,
-      location: resEmp.location,
-      country: resEmp.country,
-    };
-    return existingEmployee;
-  }
+	async getEmployeesByEmail(mail: string) {
+		const employee: unknown = await this.employeeModel.findOne({ email: mail });
+		if (employee) {
+			const resEmp: IEmployee = employee as IEmployee;
+			const existingEmployee = {
+				firstName: resEmp.firstName,
+				lastName: resEmp.lastName,
+				email: resEmp.email,
+				address1: resEmp.address1,
+				address2: resEmp.address2,
+				location: resEmp.location,
+				country: resEmp.country,
+				role: resEmp.role,
+				id: resEmp._id
+			};
+			return existingEmployee;
+		} else {
+			return null
+		}
 
-  async createEmployee(createEmployeeDto: CreateEmployeeDTO) {
-    const { email } = createEmployeeDto;
-    const user = await this.employeeModel.findOne({ email });
-    if (user) {
-      throw new HttpException(
-        'Employee already exists in the system',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-    const newEmployee = new this.employeeModel(createEmployeeDto);
-    await newEmployee.save();
-    const emp = {
-      firstName: newEmployee.firstName,
-      lastName: newEmployee.lastName,
-      email: newEmployee.email,
-      address1: newEmployee.address1,
-      address2: newEmployee.address2,
-      location: newEmployee.location,
-      country: newEmployee.country,
-      role: newEmployee.role,
-    };
-    return emp;
-  }
+	}
 
-  async updateEmployee(
-    updateEmployeeDto: UpdateEmployeeDto,
-    employeeEmail: string,
-  ) {
-    const updatedId = await this.employeeModel.updateOne(
-      {
-        email: employeeEmail,
-      },
-      updateEmployeeDto,
-    );
-    console.log(updatedId);
-    return 1;
-  }
+	async createEmployee(createEmployeeDto: CreateEmployeeDTO) {
+		const { email } = createEmployeeDto;
+		const user = await this.employeeModel.findOne({ email });
+		if (user) {
+			throw new HttpException(
+				'Employee already exists in the system',
+				HttpStatus.BAD_REQUEST,
+			);
+		}
+		const newEmployee = new this.employeeModel(createEmployeeDto);
+		await newEmployee.save();
+		const emp = {
+			firstName: newEmployee.firstName,
+			lastName: newEmployee.lastName,
+			email: newEmployee.email,
+			address1: newEmployee.address1,
+			address2: newEmployee.address2,
+			location: newEmployee.location,
+			country: newEmployee.country,
+			role: newEmployee.role,
+		};
+		return emp;
+	}
 
-  async findByLogin(UserDTO: LoginDTO) {
-    const { email, password } = UserDTO;
-    const employee = await this.employeeModel.findOne({ email });
-    if (!employee) {
-      throw new HttpException('Employee doesnt exists', HttpStatus.BAD_REQUEST);
-    }
-    if (await bcrypt.compare(password, employee.password)) {
-      const sanitized = employee.toObject();
-      delete sanitized['password'];
-      return employee;
-    } else {
-      throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
-    }
-  }
+	async updateEmployee(
+		updateEmployeeDto: UpdateEmployeeDto,
+		employeeEmail: string,
+	) {
+		const updatedId = await this.employeeModel.updateOne(
+			{
+				email: employeeEmail,
+			},
+			updateEmployeeDto,
+		);
+		console.log(updatedId);
+		return 1;
+	}
+
+	async deleteEmployee(
+		employeeEmail: string,
+	) {
+		try {
+			console.log(employeeEmail);
+			const deletedId = await this.employeeModel.deleteOne(
+				{
+					email: employeeEmail,
+				}
+			);
+			console.log(deletedId);
+			return deletedId;
+		} catch (err) {
+			throw new HttpException(err.message, 500)
+		}
+
+	}
+
+
+
+	async findByLogin(UserDTO: LoginDTO) {
+		const { email, password } = UserDTO;
+		const employee = await this.employeeModel.findOne({ email });
+		if (!employee) {
+			throw new HttpException('Employee doesnt exists', HttpStatus.NOT_FOUND);
+		}
+		if (await bcrypt.compare(password, employee.password)) {
+			const sanitized = employee.toObject();
+			delete sanitized['password'];
+			return employee;
+		} else {
+			throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+		}
+	}
 }
